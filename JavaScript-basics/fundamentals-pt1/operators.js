018f22e253379b63dd68200961c7193ec1dfc472
diegocrescini <diegocrescini@gmail.com>
2021-05-13 23:06:28 -0300

    Finished JavaScript Basics, pt. 1

---
+++ JavaScript-basics/fundamentals-pt1/operators.js
@@ -0,0 +1,20 @@
+// OPERATORS
+
+// https://javascript.info/operators
+
+// let a =2;
+// let x = 1 + (a *= 2);
+
+// " 10"
+// -1
+// 1
+// 2
+// 6
+// 9px
+// $45
+// 2
+// 4px-2 NaN
+// " -9 5"
+// -14
+// Null / NaN
+// NAN
--- JavaScript-basics/fundamentals-pt1/variables.js
+++ JavaScript-basics/fundamentals-pt1/variables.js
@@ -1,6 +1,8 @@
-//TASKS
+// TASKS
+// SOURCE https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Test_your_skills:_Math
 
-//Working with variables
+// MATH 1
+// Working with variables
 
 // Declare two variables: admin and name.
 // Assign the value "John" to name.
@@ -12,7 +14,7 @@ personName = 'John';
 admin = personName;
 // alert(admin);
 
-//Giving the right name
+// Giving the right name
 
 // Create a variable with the name of our planet. How would you name such a variable?
 // Create a variable to store the name of a current visitor to a website. How would you name that variable?
@@ -49,20 +51,105 @@ let currentVisitor; //CORRECTION: currentVisitorName or currentUserName would be
 // Finally, write a calculation that checks whether finalResult is an even number.
 // Store the result in a variable called evenOddResult.
 
-let number1 = 2;
-let number2 = 4;
-let number3 = 11;
-let number4 = 3;
-let result1 = number1 + number2;
-console.log(result1);
-let result2 = number3 - number4;
-console.log(result2);
-let finalResult = result1 * result2;
-console.log(finalResult);
-evenOddResult = finalResult % 2;
-console.log(evenOddResult);
-if (evenOddResult === 0) {
-    console.log('Eureka');
-} else {console.log('FAIL');}
+// let number1 = 2;
+// let number2 = 4;
+// let number3 = 11;
+// let number4 = 3;
+// let result1 = number1 + number2;
+// console.log(result1);
+// let result2 = number3 - number4;
+// console.log(result2);
+// let finalResult = result1 * result2;
+// console.log(finalResult);
+// evenOddResult = finalResult % 2;
+// console.log(evenOddResult);
+// if (evenOddResult === 0) {
+//     console.log('Eureka');
+// } else {console.log('FAIL');}
 
-// CONTINUE WITH https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Test_your_skills:_Math
+// MATH 2
+// In the second task you are already provided with two calculations, 
+// with the results stored in the variables result and result2. 
+// But these results aren't what we want; you'll need to take the calculations 
+// and change them to give us what we want.
+
+// What do we want? After multiplying the two results together and formatting
+// the result to 2 decimal places, the final result should be 10.42.
+
+// Try updating the live code below to recreate the finished example, 
+// following these steps:
+
+// Write a calculation that multiplies result and result2 together and assigns 
+// the result back to result. This will require assignment shorthand.
+// Write a line of code that takes result and formats it to 2 decimal places,
+// storing the result of this in a variable called finalResult.
+// Check the data type of finalResult using typeof; you'll probably 
+// see that it is actually of string type! Write a line of code that converts
+// it to a number type, storing the result in a variable called finalNumber.
+// The value of finalNumber needs to be 10.42. Go back and update the original
+// calculations you were provided with so that they give this final result.
+// Don't update the numbers or the operators.
+
+// let result = (7 + 13) / (9 + 7);
+// console.log(result);
+
+// let result2 = 100 / (2 * 6);
+// console.log(result2);
+
+// result *= result2;
+// console.log(result);
+
+// let finalResult = (Math.round(result * 100) / 100).toFixed(2);
+// console.log(finalResult);
+// console.log(typeof finalResult);
+
+// finalNumber = parseInt(finalResult, 10);
+// console.log(finalNumber);
+
+// MATH 3
+
+// In the final task for this article, we want you to write some tests. 
+// You've got three groups, each consisting of a statement and two variables.
+// For each one, write a test that proves or disproves the statement made.
+// Store the results of those tests in variables called weightComparison, 
+// heightComparison, and pwdMatch, respectively.
+
+// Statement 1: The elephant weights less than the mouse
+// let weightComparison;
+
+// let heightComparison;
+
+// let pwdMatch;
+
+
+// let eleWeight = 1000;
+// let mouseWeight = 2;
+
+// if (eleWeight < mouseWeight === true) {
+//     weightComparison = true;
+// } else {
+//     weightComparison = false;
+// }
+// console.log(weightComparison);
+
+// let ostrichHeight = 2;
+// let duckHeight = 0.3;
+
+// if(ostrichHeight > duckHeight === true) {
+//     heightComparison = true;
+// } else {
+//     heightComparison = false;
+// }
+// console.log(heightComparison);
+
+// let pwd1 = 'stromboli';
+// let pwd2 = 'stROmBoLi'
+
+// if(pwd1 === pwd2) {
+//     pwdMatch = true;
+// } else {
+//     pwdMatch = false;
+// }
+// console.log(pwdMatch);
+
+
